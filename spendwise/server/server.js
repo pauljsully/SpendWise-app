029b04d57b4fad20f6f83b091c64e9064c5609a7
peter kim <peterkim1025@gmail.com>
2024-01-29 20:21:39 -0500

    update on structure

--- spendwise/server/server.js
+++ spendwise/server/server.js
@@ -1,41 +1,27 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const PORT = process.env.PORT
 
-const startApolloServer = async () => {
-  await server.start();
-  
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  
-  app.use('/graphql', expressMiddleware(server));
+//middlewares
+app.use(express.json())
+app.use(cors())
 
-  // if we're in production, serve client/dist as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  } 
-
-  db.once('open', () => {
+const server = () => {
+    db()
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-    });
-  });
-};
+        console.log('listening to port:', PORT)
+    })
+}
 
-startApolloServer();
+server()
+
+//TODO
